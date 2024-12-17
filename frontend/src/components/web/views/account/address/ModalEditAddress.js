e6245497a930e0ca66b35a3ef94e4ed0418e37da
cuongalienat <cuongalienat@gmail.com>
2024-12-17 21:30:36 +0700

    fix phone bug

--- frontend/src/components/web/views/account/address/ModalEditAddress.js
+++ frontend/src/components/web/views/account/address/ModalEditAddress.js
@@ -19,26 +19,26 @@ const formValid = ({ formErrors, ...rest }) => {
     return valid;
 };
 const phoneRegex = RegExp(
-    /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]‌​)\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]‌​|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})\s*(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+)\s*)?$/
+    /^(?:\+84|0)(3[2-9]|5[2-9]|7[0-9]|8[1-9]|9[0-9])\d{7}$/
 )
-const ModalEditAddress = (props)=>{
+const ModalEditAddress = (props) => {
 
-    const [arrInput,setArrInput]=useState({
-        id:'',fullName:'',phoneNumber:'',ward:'',district:'',city:'',shippingAdr:'',userId:'',
-        formErrors:{
-            fullName:'',phoneNumber:'',ward:'',district:'',city:'',shippingAdr:''
+    const [arrInput, setArrInput] = useState({
+        id: '', fullName: '', phoneNumber: '', ward: '', district: '', city: '', shippingAdr: '', userId: '',
+        formErrors: {
+            fullName: '', phoneNumber: '', ward: '', district: '', city: '', shippingAdr: ''
         }
     })
-    const [empty,setEmpty]=useState(false);
+    const [empty, setEmpty] = useState(false);
 
-    
+
     const handleOnChange = (event) => {
         const { name, value } = event.target;
         let formErrors = arrInput.formErrors
         switch (name) {
             case "fullName":
                 formErrors.fullName =
-                    (value.length < 3 ) ? "Minimum 3 character required" : '';
+                    (value.length < 3) ? "Minimum 3 character required" : '';
                 break;
             case "phoneNumber":
                 formErrors.phoneNumber =
@@ -46,49 +46,49 @@ const ModalEditAddress = (props)=>{
                 break;
             case "shippingAdr":
                 formErrors.shippingAdr =
-                    (value.length < 3 ) ? "Minimum 3 character required" : '';
+                    (value.length < 3) ? "Minimum 3 character required" : '';
                 break;
             default:
                 break;
         }
         setArrInput({ ...arrInput, [name]: value });
     };
-    useEffect(()=>{
+    useEffect(() => {
         let address = props.currentAddress;
         // console.log(address)
-        if(address && !_.isEmpty(address)){
-            arrInput.id=address.id;
-            arrInput.fullName=address.fullName;
-            arrInput.phoneNumber=address.phoneNumber;
-            arrInput.ward=address.ward;
-            arrInput.district=address.district;
-            arrInput.city=address.city;
-            arrInput.shippingAdr=address.shippingAdr;
-            arrInput.userId=address.userId
-            setArrInput({...arrInput});
+        if (address && !_.isEmpty(address)) {
+            arrInput.id = address.id;
+            arrInput.fullName = address.fullName;
+            arrInput.phoneNumber = address.phoneNumber;
+            arrInput.ward = address.ward;
+            arrInput.district = address.district;
+            arrInput.city = address.city;
+            arrInput.shippingAdr = address.shippingAdr;
+            arrInput.userId = address.userId
+            setArrInput({ ...arrInput });
         }
-        
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[props.currentAddress]);
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.currentAddress]);
     console.log(arrInput);
 
-    const toggle=()=>{
+    const toggle = () => {
         props.toggleFromParent()
     }
-    const handleGetAddress = (address)=>{
+    const handleGetAddress = (address) => {
         arrInput.ward = address.ward;
         arrInput.district = address.district;
         arrInput.city = address.city;
-        setArrInput({...arrInput});
+        setArrInput({ ...arrInput });
     }
-    const handleEditAddress = async()=>{
-        const {id,fullName,phoneNumber,ward,district,city,shippingAdr,userId}=arrInput;
-        const data = {id:id,fullName:fullName,phoneNumber:phoneNumber,ward:ward,district:district,city:city,shippingAdr:shippingAdr,userId:userId}
-        if(formValid(arrInput)){
+    const handleEditAddress = async () => {
+        const { id, fullName, phoneNumber, ward, district, city, shippingAdr, userId } = arrInput;
+        const data = { id: id, fullName: fullName, phoneNumber: phoneNumber, ward: ward, district: district, city: city, shippingAdr: shippingAdr, userId: userId }
+        if (formValid(arrInput)) {
             console.log(data);
             props.editAddress(data);
             setEmpty(false);
-        }else{
+        } else {
             setEmpty(true)
             toast.error('Input empty!', {
                 position: "top-right",
@@ -102,14 +102,14 @@ const ModalEditAddress = (props)=>{
             });
         }
     }
-    return(
-        <Modal 
-            isOpen={props.isOpen} 
-            toggle={toggle} 
+    return (
+        <Modal
+            isOpen={props.isOpen}
+            toggle={toggle}
             className={props.className}
             size="lg"
             centered
-            style={{width: "600px"}}
+            style={{ width: "600px" }}
         >
             <ModalHeader toggle={toggle}>Add Address</ModalHeader>
             <ModalBody>
@@ -118,72 +118,72 @@ const ModalEditAddress = (props)=>{
                         <div className="row">
                             <div class="col-12 mb-3">
                                 <label class="form-label">Full Name</label>
-                                <input 
+                                <input
                                     type='text'
-                                     minLength={3} 
-                                     placeholder="Full Name"
+                                    minLength={3}
+                                    placeholder="Full Name"
                                     className={"form-control " + ((arrInput.formErrors.fullName.length > 0 && arrInput.fullName !== '') || (empty === true) ? 'focusError' : '')}
-                                    onChange={ handleOnChange}
+                                    onChange={handleOnChange}
                                     name="fullName"
-                                    value={arrInput.fullName} 
+                                    value={arrInput.fullName}
                                     required
                                 />
                                 {arrInput.formErrors.fullName.length > 0 && arrInput.fullName !== '' ?
-                                                <p className="text-danger my-1"><i class="fa-solid fa-circle-exclamation"></i> {arrInput.formErrors.username}</p>
-                                            : 
-                                            ''
+                                    <p className="text-danger my-1"><i class="fa-solid fa-circle-exclamation"></i> {arrInput.formErrors.username}</p>
+                                    :
+                                    ''
                                 }
                             </div>
                             <div className="col-12 mb-3">
                                 <label className="form-label">Phone number</label>
-                                    <input required
+                                <input required
                                     placeholder="Phone number"
-                                        type="text" 
-                                        className={"form-control " + ((arrInput.formErrors.phoneNumber.length > 0 && arrInput.phoneNumber !== '')||(empty === true) ? 'focusError' : '')}
-                                        onChange={ handleOnChange}
-                                        name="phoneNumber"
-                                        maxLength={11} 
-                                        pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                                        value={arrInput.phoneNumber} 
-                                    />
-                                    {arrInput.formErrors.phoneNumber.length > 0 && arrInput.phoneNumber !== '' ?
-                                        <p className="text-danger my-1"><i class="fa-solid fa-circle-exclamation"></i> {arrInput.formErrors.phoneNumber}</p>
-                                    : 
+                                    type="text"
+                                    className={"form-control " + ((arrInput.formErrors.phoneNumber.length > 0 && arrInput.phoneNumber !== '') || (empty === true) ? 'focusError' : '')}
+                                    onChange={handleOnChange}
+                                    name="phoneNumber"
+                                    maxLength={11}
+                                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                                    value={arrInput.phoneNumber}
+                                />
+                                {arrInput.formErrors.phoneNumber.length > 0 && arrInput.phoneNumber !== '' ?
+                                    <p className="text-danger my-1"><i class="fa-solid fa-circle-exclamation"></i> {arrInput.formErrors.phoneNumber}</p>
+                                    :
                                     ''
-                                    }
-                                    <div className="text-muted mt-1" style={{fontSize:"13px"}}>Keep 9-digit format with no spaces and dashes</div>
-                                </div>
+                                }
+                                <div className="text-muted mt-1" style={{ fontSize: "13px" }}>Keep 9-digit format with no spaces and dashes</div>
+                            </div>
                             <ToastContainer />
-                            <p>*<b>City:</b> {props.currentAddress.city}<br/> *<b>District:</b> {props.currentAddress.district}<br/> *<b>Ward:</b> {props.currentAddress.ward}</p>
+                            <p>*<b>City:</b> {props.currentAddress.city}<br /> *<b>District:</b> {props.currentAddress.district}<br /> *<b>Ward:</b> {props.currentAddress.ward}</p>
                             <SelectAddress onSelect={handleGetAddress} />
                             <div class="col-12 mb-4">
                                 <label class="form-label">Shipping Address</label>
-                                <textarea 
-                                    className={"form-control " + ((arrInput.formErrors.shippingAdr.length > 0 && arrInput.shippingAdr !== '')||(empty === true) ? 'focusError' : '')}
-                                    rows="2" 
-                                    name="shippingAdr" 
+                                <textarea
+                                    className={"form-control " + ((arrInput.formErrors.shippingAdr.length > 0 && arrInput.shippingAdr !== '') || (empty === true) ? 'focusError' : '')}
+                                    rows="2"
+                                    name="shippingAdr"
                                     required
-                                    onChange={ handleOnChange}
+                                    onChange={handleOnChange}
                                     value={arrInput.shippingAdr}
-                                    >
+                                >
                                     {arrInput.shippingAdr}
                                 </textarea>
                                 {arrInput.formErrors.shippingAdr.length > 0 && arrInput.shippingAdr !== '' ?
                                     <p className="text-danger my-1"><i class="fa-solid fa-circle-exclamation"></i> {arrInput.formErrors.shippingAdr}</p>
-                                : 
-                                ''
+                                    :
+                                    ''
                                 }
-                            </div>                                                   
-                        </div>            
+                            </div>
+                        </div>
                     </div>
                 </div>
             </ModalBody>
             <ModalFooter>
                 <Button color="secondary" onClick={toggle}>Cancel</Button>
-                <Button style={{color:"#ffffff",backgroundColor:"rgb(231, 76, 60)",border:"none"}} onClick={()=>{handleEditAddress()}}>Add</Button>
+                <Button style={{ color: "#ffffff", backgroundColor: "rgb(231, 76, 60)", border: "none" }} onClick={() => { handleEditAddress() }}>Add</Button>
             </ModalFooter>
         </Modal>
     )
-   
+
 }
 export default ModalEditAddress;
--- frontend/src/components/web/views/account/profile/index.js
+++ frontend/src/components/web/views/account/profile/index.js
@@ -4,7 +4,7 @@ import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
 const phoneRegex = RegExp(
-    /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]‌​)\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]‌​|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})\s*(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+)\s*)?$/
+    /^(?:\+84|0)(3[2-9]|5[2-9]|7[0-9]|8[1-9]|9[0-9])\d{7}$/
 )
 const formValid = ({ formErrors, ...rest }) => {
     let valid = true;
@@ -18,48 +18,49 @@ const formValid = ({ formErrors, ...rest }) => {
     });
     return valid;
 };
-const Profile = ()=>{
-    const [showSave,setShowSave]=useState(false);
+const Profile = () => {
+    const [showSave, setShowSave] = useState(false);
     const [getUser, setGetUser] = useState({
-        username:'',phoneNumber:'',email:'',id:'',
-        formErrors :{
-            phoneNumber:''
+        username: '', phoneNumber: '', email: '', id: '',
+        formErrors: {
+            phoneNumber: ''
         }
     });
-    const [empty,setEmpty]=useState(false);
+    const [empty, setEmpty] = useState(false);
 
-    const handleFormUpdate = ()=>{
+    const handleFormUpdate = () => {
         setShowSave(true);
     }
-   
-    const handleBack = ()=>{
+
+    const handleBack = () => {
         setShowSave(false);
     }
-    useEffect(()=>{
+    useEffect(() => {
         getUserFromReact();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[]);
-    
-    const getUserFromReact = async()=>{
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const getUserFromReact = async () => {
         let email = localStorage.getItem('email')
         if (email) {
-            let data= await getUserByEmail(email);
+            let data = await getUserByEmail(email);
             if (data && data.data.errCode === 0) {
-                setGetUser({...getUser,
-                    username:data.data.user.username,
-                    phoneNumber:data.data.user.phoneNumber,
-                    email:data.data.user.email,
-                    id:data.data.user.id,
+                setGetUser({
+                    ...getUser,
+                    username: data.data.user.username,
+                    phoneNumber: data.data.user.phoneNumber,
+                    email: data.data.user.email,
+                    id: data.data.user.id,
                 })
             }
         }
     }
-    
+
     const handleOnChange = (event) => {
         const { name, value } = event.target;
         let formErrors = getUser.formErrors
         switch (name) {
-           
+
             case "phoneNumber":
                 formErrors.phoneNumber =
                     phoneRegex.test(value) ? "" : "Invalid email phone number";
@@ -69,14 +70,14 @@ const Profile = ()=>{
         }
         setGetUser({ ...getUser, [name]: value });
     };
-    const handleSave = async()=>{
-        let { phoneNumber,id } = getUser;
-        let dataUser = {phoneNumber:phoneNumber,id:id }
+    const handleSave = async () => {
+        let { phoneNumber, id } = getUser;
+        let dataUser = { phoneNumber: phoneNumber, id: id }
         if (formValid(getUser)) {
             setEmpty(false);
             try {
                 let list = await editUserService(dataUser);
-                if(list.data && list.data.errCode !== 0 ){
+                if (list.data && list.data.errCode !== 0) {
                     toast.error(list.data.message, {
                         position: "top-right",
                         autoClose: 5000,
@@ -88,7 +89,7 @@ const Profile = ()=>{
                         theme: "light",
                     });
                 }
-                if(list.data && list.data.errCode === 0 ){
+                if (list.data && list.data.errCode === 0) {
                     toast.success('Update success!', {
                         position: "top-right",
                         autoClose: 2000,
@@ -111,9 +112,9 @@ const Profile = ()=>{
                     draggable: true,
                     progress: undefined,
                     theme: "light",
-                });  
+                });
             }
-        } else{
+        } else {
             setEmpty(true)
             toast.error('Input empty!', {
                 position: "top-right",
@@ -126,7 +127,7 @@ const Profile = ()=>{
                 theme: "light",
             });
         }
-        
+
     }
     return (
         <div className="container">
@@ -137,60 +138,60 @@ const Profile = ()=>{
                 <div className="row">
                     <div class="col-12 mb-3">
                         <label for="exampleFormControlInput1" class="form-label me-2">Username</label>
-                        {showSave && 
-                        <span className="text-muted" style={{fontSize:"13px"}}>
+                        {showSave &&
+                            <span className="text-muted" style={{ fontSize: "13px" }}>
                                 Not change
-                        </span>
+                            </span>
                         }
-                        <input type="text" value={getUser.username} class="form-control" id="exampleFormControlInput1" disabled/>
+                        <input type="text" value={getUser.username} class="form-control" id="exampleFormControlInput1" disabled />
                     </div>
-                    
+
                     <div class="col-12 mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Phone number</label>
-                        <input 
+                        <input
                             type="tel"
                             pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                            name="phoneNumber" 
+                            name="phoneNumber"
                             maxLength={11}
                             required
-                            onChange={handleOnChange} 
-                            value={getUser.phoneNumber} 
-                            disabled={showSave ? '':'1'}
+                            onChange={handleOnChange}
+                            value={getUser.phoneNumber}
+                            disabled={showSave ? '' : '1'}
                             // className="form-control"
-                            className={"form-control " + (showSave ? ((getUser.formErrors.phoneNumber.length > 0 && getUser.phoneNumber !== '') ||(empty === true) ? 'focusError' : ''):'')}
-                            />
-                        {showSave && <div className="text-muted mt-1" style={{fontSize:"13px"}}>Keep 9-digit format with no spaces and dashes</div>}
-                        
+                            className={"form-control " + (showSave ? ((getUser.formErrors.phoneNumber.length > 0 && getUser.phoneNumber !== '') || (empty === true) ? 'focusError' : '') : '')}
+                        />
+                        {showSave && <div className="text-muted mt-1" style={{ fontSize: "13px" }}>Keep 9-digit format with no spaces and dashes</div>}
+
                         {getUser.formErrors.phoneNumber.length > 0 && getUser.phoneNumber !== '' ?
-                                <p className="text-danger my-1"><i class="fa-solid fa-circle-exclamation"></i> {getUser.formErrors.phoneNumber}</p>
-                            : 
+                            <p className="text-danger my-1"><i class="fa-solid fa-circle-exclamation"></i> {getUser.formErrors.phoneNumber}</p>
+                            :
                             ''
-                            }
+                        }
                     </div>
                     <div class="col-12 mb-3">
                         <label class="form-label me-2">Email address</label>
-                        {showSave && 
-                        <span className="text-muted" style={{fontSize:"13px"}}>
+                        {showSave &&
+                            <span className="text-muted" style={{ fontSize: "13px" }}>
                                 Not change
-                        </span>
+                            </span>
                         }
-                        <input type="email" value={getUser.email} class="form-control" disabled/>
+                        <input type="email" value={getUser.email} class="form-control" disabled />
                     </div>
                     <ToastContainer />
-                    {showSave ? 
-                    <div className="d-flex justify-content-end">
-                        <button className="px-4 btn btn-secondary me-3" onClick={handleBack}>Back</button>
-                        <button className="px-4 btn" style={{color:"#ffffff",backgroundColor:"rgb(231, 76, 60)"}} onClick={handleSave}>Save</button>
-                    </div>
-                    :
-                    <div className="d-flex justify-content-end">
-                        <button className="btn px-4" style={{color:"#ffffff",backgroundColor:"rgb(231, 76, 60)"}} onClick={handleFormUpdate}>Update</button>
-                    </div>
+                    {showSave ?
+                        <div className="d-flex justify-content-end">
+                            <button className="px-4 btn btn-secondary me-3" onClick={handleBack}>Back</button>
+                            <button className="px-4 btn" style={{ color: "#ffffff", backgroundColor: "rgb(231, 76, 60)" }} onClick={handleSave}>Save</button>
+                        </div>
+                        :
+                        <div className="d-flex justify-content-end">
+                            <button className="btn px-4" style={{ color: "#ffffff", backgroundColor: "rgb(231, 76, 60)" }} onClick={handleFormUpdate}>Update</button>
+                        </div>
                     }
-                    
+
                 </div>
             </div>
-        </div>  
+        </div>
     )
 }
 export default Profile;
